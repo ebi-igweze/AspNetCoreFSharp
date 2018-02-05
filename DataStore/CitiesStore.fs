77f5d950269d0cdafdbb6ef17f2f3fbabf5177fa
ebi igweze <ebi.igweze@gmail.com>
2018-02-05 09:48:12 +0100

    adds update, delete and retrieve request actions for PointOfInterest

--- DataStore/CitiesStore.fs
+++ DataStore/CitiesStore.fs
@@ -1,5 +1,16 @@
 ﻿module CitiesStore
 
+module InputDTOs = 
+    open System.ComponentModel.DataAnnotations
+
+    [<CLIMutable>]
+    type PointOfInterest = {
+        [<Required(ErrorMessage = "Name is required."); MaxLength(50, ErrorMessage = "Name must be less than 50 characters")>]
+        name: string
+        [<Required(ErrorMessage = "Description is a required input"); MaxLength(200, ErrorMessage = "Description must be less than 200 characters")>]
+        description: string
+    }
+    
 type PointOfInterest = {
     id: int
     name: string
@@ -11,22 +22,22 @@ type City = {
     id: int;
     name:string;
     description: string;
-    numberOfPointsOfInterest: int;
-    pointsOfInterest: PointOfInterest[]
+    numberOfPointsOfInterest: unit -> int;
+    pointsOfInterest: PointOfInterest ResizeArray
 }
 
 let private pointsOfInterest = [
-    [| {id = 1; name = "Central Park"; description = "The most visited park in the US" }
-       {id = 2; name = "Empire State Building"; description = "A 102 story skyscraper located in Midtown Mahattan."} |];
-    [| {id = 3; name = "Banana Island"; description = "The most expensive place to live on the Lagos Island"}
-       {id = 4; name = "IMax Studios"; description = "The cinema with the biggest screen display in Nigeria"} |]
-    [| {id = 5; name = "Aso Rock"; description = "The rock on which the President of Nigeria lives"} |]
+    ResizeArray([| {id = 1; name = "Central Park"; description = "The most visited park in the US" }
+                   {id = 2; name = "Empire State Building"; description = "A 102 story skyscraper located in Midtown Mahattan."} |]);
+    ResizeArray([| {id = 3; name = "Banana Island"; description = "The most expensive place to live on the Lagos Island"}
+                   {id = 4; name = "IMax Studios"; description = "The cinema with the biggest screen display in Nigeria"} |]);
+    ResizeArray([| {id = 5; name = "Aso Rock"; description = "The rock on which the President of Nigeria lives"} |])
 ]
 let private cities = 
     [|
-        { id=1; name="New York City"; description="The Apple with the red color"; pointsOfInterest = pointsOfInterest.[0]; numberOfPointsOfInterest = pointsOfInterest.[0].Length}
-        { id=2; name="Lagos"; description="The Apple of Nigeria"; pointsOfInterest = pointsOfInterest.[1]; numberOfPointsOfInterest = pointsOfInterest.[1].Length}
-        { id=3; name="Abuja"; description="The Secretariat of Nigeria"; pointsOfInterest = pointsOfInterest.[2]; numberOfPointsOfInterest = pointsOfInterest.[2].Length} |]
+        { id=1; name="New York City"; description="The Apple with the red color"; pointsOfInterest = pointsOfInterest.[0]; numberOfPointsOfInterest = fun () -> pointsOfInterest.[0].Count}
+        { id=2; name="Lagos"; description="The Apple of Nigeria"; pointsOfInterest = pointsOfInterest.[1]; numberOfPointsOfInterest = fun () -> pointsOfInterest.[1].Count}
+        { id=3; name="Abuja"; description="The Secretariat of Nigeria"; pointsOfInterest = pointsOfInterest.[2]; numberOfPointsOfInterest = fun () -> pointsOfInterest.[2].Count} |]
 
 
 let getCities (): City[] = cities
@@ -38,5 +49,18 @@ let getCity id: City option =
             select city
     } |> Seq.tryHead
 
+let getPointOfInterest cityId id: PointOfInterest option =
+    getCity cityId
+    |> Option.bind (fun city -> 
+        query {
+            for poi in city.pointsOfInterest do
+                where (poi.id = id)
+                select poi
+            } |> Seq.tryHead )
+
+let removePointOfInterest cityId poi: bool option =
+    getCity cityId
+    |> Option.map (fun city -> city.pointsOfInterest.Remove(poi))
+    
     
     
--- controllers/CitiesController.fs
+++ controllers/CitiesController.fs
@@ -1,6 +1,9 @@
 namespace AspNetCoreFSharp.API.Controllers
 
 open Microsoft.AspNetCore.Mvc
+open CitiesStore
+open Microsoft.AspNetCore.Mvc.ModelBinding
+open Microsoft.AspNetCore.JsonPatch
 
 [<Route("api/cities")>]
 type CitiesController() =
@@ -17,11 +20,25 @@ type CitiesController() =
         | Some city -> this.Ok city :> _
         | None -> this.NotFound () :> _
         
-        
+  
+  
+type CreatedPointOfInterestResponse = { cityId: int; id: int }
 
  [<Route("api/cities")>]
 type PointsOfInterestControler() =
     inherit Controller()
+
+    member private this.validateInput(poiOption: InputDTOs.PointOfInterest option, action: unit -> IActionResult): IActionResult = 
+        do match poiOption with 
+           | None -> ()
+           | Some poi -> 
+              if (poi.name = poi.description) 
+              then do this.ModelState.AddModelError("description", "The provided description should be different from the name")
+
+        match this.ModelState.IsValid with
+        | false -> this.BadRequest (this.ModelState) :> _
+        | true -> action()
+            
 
     [<HttpGet("{cityId}/pointsOfInterest")>]
     member this.getPointsOfInterest (cityId: int): IActionResult =
@@ -29,20 +46,49 @@ type PointsOfInterestControler() =
         | Some city -> this.Ok city.pointsOfInterest :> _
         | None -> this.NotFound() :> _
         
-    [<HttpGet("{cityId}/pointsOfInterest/{id}")>]
+    [<HttpGet("{cityId}/pointsOfInterest/{id}", Name="GetPointOfInterest")>]
     member this.getPointOfInterest (cityId: int, id: int): IActionResult =
-        match CitiesStore.getCity(cityId) with
+        match CitiesStore.getPointOfInterest cityId id with
         | None -> this.NotFound() :> _
-        | Some city -> 
-            let result = 
-                query {
-                    for poi in city.pointsOfInterest do
-                        where (poi.id = id)
-                        select poi
-                    } |> Seq.tryHead
-            match result with
-            | Some poi -> this.Ok poi :> _
+        | Some poi ->  this.Ok poi :> _
+
+
+    [<HttpPost("{cityId}/pointsOfInterest")>]
+    member this.createPointOfInterest (cityId: int, [<FromBody>] poi: InputDTOs.PointOfInterest): IActionResult = 
+        this.validateInput(Some poi, fun () -> 
+            match CitiesStore.getCity(cityId) with
             | None -> this.NotFound() :> _
+            | Some city -> 
+                let newPoi = { id = city.numberOfPointsOfInterest() + 1; name = poi.name; description = poi.description}
+                do city.pointsOfInterest.Add newPoi
+                this.CreatedAtRoute("GetPointOfInterest", { cityId = cityId; id = newPoi.id }, newPoi) :> _ )
 
+     [<HttpPut("{cityId}/pointsOfInterest/{id}")>]
+     member this.updatePointsOfInterest (cityId: int, id: int, [<FromBody>] poi: InputDTOs.PointOfInterest): IActionResult = 
+        this.validateInput (Some poi, fun () -> 
+            match CitiesStore.getPointOfInterest cityId id with
+            | None -> this.NotFound() :> _
+            | Some p -> 
+                let v =  { p with name = poi.name; description = poi.description }
+                this.NoContent() :> _ )
 
+     [<HttpPatch("{cityId}/pointsOfInterest/{id}")>]
+     member this.patchPointOfInterest (cityId: int, id: int, [<FromBody>] poiPatchDoc: JsonPatchDocument<InputDTOs.PointOfInterest>): IActionResult = 
+        this.validateInput(None, fun () -> 
+            match CitiesStore.getPointOfInterest cityId id with
+            | None -> this.NotFound () :> _
+            | Some poi -> 
+                let poiToEdit = { InputDTOs.PointOfInterest.name = poi.name; InputDTOs.PointOfInterest.description = poi.description }
+                do poiPatchDoc.ApplyTo(poiToEdit, this.ModelState)
+                if this.ModelState.IsValid
+                then this.NoContent () :> _
+                else this.BadRequest(this.ModelState) :> _ )
      
+     [<HttpDelete("{cityId}/pointsOfInterest/{id}")>]
+     member this.deletePointOfInterest(cityId: int, id: int): IActionResult =
+        match CitiesStore.getPointOfInterest cityId id with
+        | None -> this.NotFound() :> _
+        | Some poi -> 
+            match CitiesStore.removePointOfInterest cityId poi with
+            | Some _ -> this.NoContent () :> _
+            | None -> this.BadRequest() :> _
